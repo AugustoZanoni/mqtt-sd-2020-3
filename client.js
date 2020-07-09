345095c52a13399f7d1fe5b6b5b4d82ad4111740
Augusto <augustozanonii@gmail.com>
2020-07-08 22:56:06 -0300

    primeiras mudanças

--- client.js
+++ client.js
@@ -4,6 +4,14 @@ var mqtt = require('mqtt');
 // conexão estabelecida com o broker mqtt do servidor de teste do mosquitto
 var client  = mqtt.connect('mqtt://test.mosquitto.org');
 
+
+const LISTA_CARDAPIO = 'lista-cardapio',
+    LISTA_CARRINHO = 'lista-carrinho',
+    PEGA_ITEM = 'pega-item',
+    REMOVE_ITEM = 'remove-item',
+    NOVO_ITEM_CARDAPIO = 'novo-item';
+
+
 // descrição do carro1
 const carro1 = {
   modelo: "versa",
@@ -11,44 +19,33 @@ const carro1 = {
   cor: "cinza",
 };
 
-// descrição do carro2
-const carro2 = {
-  modelo: "gol",
-  marca: "volkswagen",
-  cor: "vermelho",
-};
-
-// descrição do carro3
-const carro3 = {
-  modelo: "uno",
-  marca: "fiat",
-  cor: "branco",
-};
-
 // adicionado o listener para o evento 'connect' (que executa quando a conexão é estabelecida)
 client.on('connect', function() {
 
-  // publica os dados do carro1 no tópico "registra-carro"
-  client.publish('registra-carro', JSON.stringify(carro1));
+  client.publish(PEGA_ITEM, '3');
+  client.publish(LISTA_CARRINHO, "");
+  // // publica os dados do carro1 no tópico "registra-carro"
+  // client.publish('registra-carro', JSON.stringify(carro1));
 
-  // publica os dados do carro2 no tópico "registra-carro"
-  client.publish('registra-carro', JSON.stringify(carro2));
+  // // publica os dados do carro2 no tópico "registra-carro"
+  // client.publish('registra-carro', JSON.stringify(carro2));
 
-  // publica os dados do carro3 no tópico "registra-carro"
-  client.publish('registra-carro', JSON.stringify(carro3));
+  // // publica os dados do carro3 no tópico "registra-carro"
+  // client.publish('registra-carro', JSON.stringify(carro3));
 
   // o cliente subscreve no tópico "resultado-lista-carros" para obter os resultados da requisição "lista-carros"
-  client.subscribe('resultado-lista-carros', function (err) {
+  client.subscribe(LISTA_CARDAPIO, function (err) {
     if (!err) {
-      console.log("Subscrito no tópico 'resultado-lista-carros' com sucesso!");
+      console.log(`Subscrito no tópico ${LISTA_CARDAPIO} com sucesso!`);
 
       // se tudo deu certo, após imprimir uma mensagem na tela, publica uma requisição "lista-carros"
       // ATENÇÃO! APESAR DE ESTAR USANDO A IDEIA DE CLIENTE E SERVIDOR, ESSE PARADIGMA NÃO É PARA SER
       // USADO EM PRODUÇÃO DESSA FORMA, POIS TODOS OS PROCESSOS SUBSCRITOS NO TÓPICO "lista-carros"
       // RESPONDERÃO À REQUISIÇÃO
-      client.publish('lista-carros', "");
+      
     }
   });
+  //client.publish(LISTA_CARDAPIO, "");
 
 });
 
@@ -56,9 +53,9 @@ client.on('connect', function() {
 client.on('message', function (topic, message) {
 
   // o cliente testa se a mensagem foi recebida no tópico "resultado-lista-carros"
-  if (topic === "resultado-lista-carros") {
+  if (topic === LISTA_CARDAPIO) {
     // recuperada a lista de carros como um objeto
-    const lista = JSON.parse(message.toString());
+    const lista = message.toString();
 
     // lista é impressa e a conexão é encerrada
     console.log(lista);
@@ -66,7 +63,6 @@ client.on('message', function (topic, message) {
   }
 
 });
-
 
 // .publish('resultado-consulta-carro', JSON.stringify(listaDeCarros[nrCarro]));
 //             break;
--- server.js
+++ server.js
@@ -2,67 +2,104 @@
 var mqtt = require('mqtt');
 
 // conexão estabelecida com o broker mqtt do servidor de teste do mosquitto
-var server  = mqtt.connect('mqtt://test.mosquitto.org');
+var server = mqtt.connect('mqtt://test.mosquitto.org');
 
 // constantes definidas com os nomes dos tópicos utilizados pelo processo
 const REGISTRA_CARRO = 'registra-carro';
 const CONSULTA_CARRO = 'consulta-carro';
 const LISTA_CARROS = 'lista-carros';
 
+const LISTA_CARDAPIO = 'lista-cardapio',
+    LISTA_CARRINHO = 'lista-carrinho',
+    PEGA_ITEM = 'pega-item',
+    REMOVE_ITEM_CARDAPIO = 'remove-item',
+    NOVO_ITEM_CARDAPIO = 'novo-item';
+
+
+
 // vetor que representa o "banco de dados" de carros
-const listaDeCarros = [];
+let cardapio = [
+
+    { id: 1, item: "Rafael's Cheddar Burguer", preco: "30.90" },
+    { id: 2, item: "Rafael's Bacon Burguer", preco: "28.90" },
+    { id: 3, item: "Rafael's Classic Burguer", preco: "25.90" },
+    { id: 4, item: "Macarronada Grande", preco: "25.90" },
+    { id: 5, item: "Macarronada Média", preco: "20.90" },
+    { id: 6, item: "Macarronada Pequena", preco: "15.90" }
+
+],
+    carrinho = [];
 
 // adicionado o listener para o evento 'connect' (que executa quando a conexão é estabelecida)
-server.on('connect', function() {
+server.on('connect', function () {
     // o processo subscreve em um tópico e, caso não ocorram erros, imprime uma mensagem na tela
-    server.subscribe(REGISTRA_CARRO, function (err) {
-        if (!err) {
-            console.log("Subscrito no tópico '" + REGISTRA_CARRO + "' com sucesso!");
-        }
-    });
 
-    // o processo subscreve em um tópico e, caso não ocorram erros, imprime uma mensagem na tela
-    server.subscribe(CONSULTA_CARRO, function (err) {
+    server.subscribe(LISTA_CARDAPIO, (err) => {
         if (!err) {
-            console.log("Subscrito no tópico '" + CONSULTA_CARRO + "' com sucesso!");
+            console.log('Subscrito com Sucesso em Lista Cardápio');
         }
-    });
+    })
 
-    // o processo subscreve em um tópico e, caso não ocorram erros, imprime uma mensagem na tela
-    server.subscribe(LISTA_CARROS, function (err) {
+    server.subscribe(PEGA_ITEM, (err) => {
         if (!err) {
-            console.log("Subscrito no tópico '" + LISTA_CARROS + "' com sucesso!");
+            console.log('Subscrito com Sucesso em Pega Item');
         }
-    });
+    })
+
+
+
+    // server.subscribe(REGISTRA_CARRO, function (err) {
+    //     if (!err) {
+    //         console.log("Subscrito no tópico '" + REGISTRA_CARRO + "' com sucesso!");
+    //     }
+    // });
+
+    // // o processo subscreve em um tópico e, caso não ocorram erros, imprime uma mensagem na tela
+    // server.subscribe(CONSULTA_CARRO, function (err) {
+    //     if (!err) {
+    //         console.log("Subscrito no tópico '" + CONSULTA_CARRO + "' com sucesso!");
+    //     }
+    // });
+
+    // // o processo subscreve em um tópico e, caso não ocorram erros, imprime uma mensagem na tela
+    // server.subscribe(LISTA_CARROS, function (err) {
+    //     if (!err) {
+    //         console.log("Subscrito no tópico '" + LISTA_CARROS + "' com sucesso!");
+    //     }
+    // });
 });
 
 // adicionado o listener para o evento 'message' (que executa quando uma mensagem é recebida)
-server.on('message', function(topic, message) {
+server.on('message', function (topic, message) {
 
     // o cliente testa em qual tópico a mensagem foi recebida
-    switch(topic) {
-        case REGISTRA_CARRO:
+    switch (topic) {
+        case LISTA_CARDAPIO:
+            server.publish(LISTA_CARDAPIO, JSON.stringify(cardapio));
+            break;
+        case PEGA_ITEM:
             // caso seja uma mensagem de registro, adiciona no "banco de dados"
-            const carro = JSON.parse(message);
-
-            listaDeCarros.push(carro);
+            const id = parseInt(message);
+            console.log('Função Pega Item com ID: '+id)
+            //find
             break;
 
-        case CONSULTA_CARRO:
-            // caso seja uma mensagem de consulta de um carro, retorna-o no tópico "resultado-consulta-carro"
-            const nrCarro = parseInt(message);
-            server.publish('resultado-consulta-carro', JSON.stringify(listaDeCarros[nrCarro]));
+        case NOVO_ITEM_CARDAPIO:
+            let novoItem = JSON.parse(message);
+            console.log('função novo item no dardápio: '+ item)
             break;
-            
-        case LISTA_CARROS:
+
+        case LISTA_CARRINHO:
             // caso seja uma mensagem de listagem de carros, retorna a lista completa no tópico "resultado-lista-carros"
-            console.log("LISTA DE CARROS");
+            console.log("Lista Carrinho");
 
-            server.publish('resultado-lista-carros', JSON.stringify(listaDeCarros));
-            break;        
+            server.publish('LISTA_CARRINHO', JSON.stringify(carrinho));
+            break;
+        case REMOVE_ITEM_CARDAPIO:
+            break
     }
 
     // sempre imprime a mensagem recebida
-    console.log(message.toString());
+    //console.log(message.toString());
 });
 
